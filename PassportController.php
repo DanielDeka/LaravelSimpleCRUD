90713f8bcaf6d70eb3c398b8596451311f9c5215
Daniel Kurniawan <dekaslr8@gmail.com>
2018-11-09 15:06:39 +0700

    Create PassportController.php

---
+++ PassportController.php
@@ -0,0 +1,28 @@
+//PassportController.php
+/**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if($request->hasfile('filename'))
+         {
+            $file = $request->file('filename');
+            $name=time().$file->getClientOriginalName();
+            $file->move(public_path().'/images/', $name);
+         }
+        $passport= new \App\Passport;
+        $passport->name=$request->get('name');
+        $passport->email=$request->get('email');
+        $passport->number=$request->get('number');
+        $date=date_create($request->get('date'));
+        $format = date_format($date,"Y-m-d");
+        $passport->date = strtotime($format);
+        $passport->office=$request->get('office');
+        $passport->filename=$name;
+        $passport->save();
+        
+        return redirect('passports')->with('success', 'Information has been added');
+    }
